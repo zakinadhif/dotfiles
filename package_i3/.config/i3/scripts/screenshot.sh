b58f3f20657a2ad86c513c3e5f8e9cfd1f4d2430
cowdingus <zaki.nadhif45@gmail.com>
2022-02-28 22:10:27 +0700

    Replace screenshot.sh with maim replacement

--- package_i3/.config/i3/scripts/screenshot.sh
+++ package_i3/.config/i3/scripts/screenshot.sh
@@ -2,20 +2,24 @@
 
 case "$1" in
     all_save)
-        scrot '%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f $$(xdg-user-dir PICTURES)'
+        maim $(xdg-user-dir PICTURES)/$(date +%Y-%m-%d-%s_screenshot.jpg)
         dunstify "Screenshot has been saved to `xdg-user-dir PICTURES`"
         ;;
     window_save)
-        scrot '%Y-%m-%d-%s_screenshot_$wx$h.jpg' -u -e 'mv $f $$(xdg-user-dir PICTURES)'
+        maim -st 9999999 $(xdg-user-dir PICTURES)/$(date +%Y-%m-%d-%s_screenshot_window.jpg)
         dunstify "Screenshot has been saved to `xdg-user-dir PICTURES`"
+        ;;
+    window_save_fancy)
+        dunstify "Screenshot has been saved to `xdg-user-dir PICTURES`"
+        maim -st 9999999 | convert - \( +clone -background black -shadow 80x3+5+5 \) +swap -background none -layers merge +repage $(xdg-user-dir PICTURES)/$(date +%Y-%m-%d-%s_screenshot_fancy.jpg)
         ;;
     selection_save)
         dunstify "Selection will be saved to `xdg-user-dir PICTURES`"
-        scrot '%Y-%m-%d-%s_screenshot_$wx$h.jpg' -s -e 'mv $f $$(xdg-user-dir PICTURES)'
+        maim -s $(xdg-user-dir PICTURES)/$(date +%Y-%m-%d-%s_screenshot_selection.jpg)
         ;;
     selection_copy)
-        dunstify 'Selection will be saved to /tmp/tmp_scrot'
-        scrot -s -e 'mv $f /tmp/tmp_scrot && xclip -selection clipboard -t image/png -i /tmp/tmp_scrot'
+        dunstify 'Selection will be copied into clipboard'
+        maim -s | xclip -selection clipboard -t image/png
         ;;
     *)
         echo "Usage: $0 {all_save|selection_save|window_save|all_copy|selection_copy|window_copy}"
